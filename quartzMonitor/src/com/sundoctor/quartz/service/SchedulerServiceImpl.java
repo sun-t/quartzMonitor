e522d84f9bdb7d680a6bd625691a7dc7838229c8
taotaojudy <bupt622@126.com>
2014-08-18 18:15:54 +0800

    third commit
    Signed-off-by: taotaojudy <bupt622@126.com>

--- quartzMonitor/src/com/sundoctor/quartz/service/SchedulerServiceImpl.java
+++ quartzMonitor/src/com/sundoctor/quartz/service/SchedulerServiceImpl.java
@@ -32,6 +32,7 @@ public class SchedulerServiceImpl implements SchedulerService {
 	private JobDetail jobDetail;
 	private QuartzDao quartzDao;
 	////
+	
 	private static final Logger logger = LoggerFactory.getLogger(SchedulerServiceImpl.class);
 	
 	//Autowired spring可以自动帮你把bean里面引用的对象的setter/getter方法省略，它会自动帮你set/get
